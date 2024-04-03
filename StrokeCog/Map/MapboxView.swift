b968fc2071dd3d1d2809fce951e7a4dfa55213cb
Vishnu Ravi <vishnur@stanford.edu>
2024-04-03 12:49:35 -0400

    Fix swiftlint warnings

--- StrokeCog/Map/MapboxView.swift
+++ StrokeCog/Map/MapboxView.swift
@@ -48,6 +48,7 @@ public class MapManagerView: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
