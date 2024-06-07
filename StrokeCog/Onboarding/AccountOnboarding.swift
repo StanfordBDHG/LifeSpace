fff9f43cfc2be4b7aafbad585f771eddff150df3
Vishnu Ravi <vishnur@stanford.edu>
2024-06-07 11:28:40 -0400

    Fix duplicate account setup header

--- StrokeCog/Onboarding/AccountOnboarding.swift
+++ StrokeCog/Onboarding/AccountOnboarding.swift
@@ -18,49 +18,52 @@ struct AccountOnboarding: View {
     
     var body: some View {
         VStack {
-            AccountSetupHeader()
-                .padding()
-            AccountSetup { _ in
-                Task {
-                    // Placing the nextStep() call inside this task will ensure that the sheet dismiss animation is
-                    // played till the end before we navigate to the next step.
-                    onboardingNavigationPath.nextStep()
-                }
-            } continue: {
-                OnboardingActionsView(
-                    "ACCOUNT_NEXT",
-                    action: {
+            AccountSetup(
+                setupComplete: { _ in
+                    Task {
+                        // Placing the nextStep() call inside this task will ensure that the sheet dismiss animation is
+                        // played till the end before we navigate to the next step.
                         onboardingNavigationPath.nextStep()
                     }
-                )
-            }
+                },
+                header: {
+                    AccountSetupHeader()
+                },
+                continue: {
+                    OnboardingActionsView(
+                        "ACCOUNT_NEXT",
+                        action: {
+                            onboardingNavigationPath.nextStep()
+                        }
+                    )
+                }
+            )
         }
     }
 }
-
-
+    
 #if DEBUG
-#Preview("Account Onboarding SignIn") {
-    OnboardingStack {
-        AccountOnboarding()
-    }
+    #Preview("Account Onboarding SignIn") {
+        OnboardingStack {
+            AccountOnboarding()
+        }
         .previewWith {
             AccountConfiguration {
                 MockUserIdPasswordAccountService()
             }
         }
-}
-
-#Preview("Account Onboarding") {
-    let details = AccountDetails.Builder()
-        .set(\.userId, value: "lelandstanford@stanford.edu")
-        .set(\.name, value: PersonNameComponents(givenName: "Leland", familyName: "Stanford"))
-    
-    return OnboardingStack {
-        AccountOnboarding()
     }
+    
+    #Preview("Account Onboarding") {
+        let details = AccountDetails.Builder()
+            .set(\.userId, value: "lelandstanford@stanford.edu")
+            .set(\.name, value: PersonNameComponents(givenName: "Leland", familyName: "Stanford"))
+        
+        return OnboardingStack {
+            AccountOnboarding()
+        }
         .previewWith {
             AccountConfiguration(building: details, active: MockUserIdPasswordAccountService())
         }
-}
+    }
 #endif
