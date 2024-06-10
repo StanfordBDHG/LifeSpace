5fcdfbf662d9fe34aeb0b7841177889e70183640
Vishnu Ravi <vishnur@stanford.edu>
2024-06-10 00:42:18 -0400

    Switch to RK consent and add HIPAA form

--- StrokeCog/Onboarding/Consent.swift
+++ StrokeCog/Onboarding/Consent.swift
@@ -6,33 +6,73 @@
 // SPDX-License-Identifier: MIT
 //
 
+import ResearchKit
+import ResearchKitSwiftUI
 import SpeziOnboarding
 import SwiftUI
 
 
-/// - Note: The `OnboardingConsentView` exports the signed consent form as PDF to the Spezi `Standard`, necessitating the conformance of the `Standard` to the `OnboardingConstraint`.
 struct Consent: View {
     @Environment(OnboardingNavigationPath.self) private var onboardingNavigationPath
-    
-    
-    private var consentDocument: Data {
-        guard let path = Bundle.main.url(forResource: "ConsentDocument", withExtension: "md"),
-              let data = try? Data(contentsOf: path) else {
-            return Data(String(localized: "CONSENT_LOADING_ERROR").utf8)
-        }
-        return data
-    }
-    
-    
+    @State private var isConsentSheetPresented = false
+
     var body: some View {
-        OnboardingConsentView(
-            markdown: {
-                consentDocument
-            },
-            action: {
+        Button(action: {
+            isConsentSheetPresented = true
+        }) {
+            Text("Show Consent")
+        }
+        .sheet(isPresented: $isConsentSheetPresented) {
+            ORKOrderedTaskView(tasks: consentTask) { result in
+                guard case let .completed(taskResult) = result else {
+                    self.isConsentSheetPresented = false
+                    return // user cancelled or task failed
+                }
+
+                self.isConsentSheetPresented = false
                 onboardingNavigationPath.nextStep()
             }
+            .ignoresSafeArea(edges: .all)
+        }
+        .onAppear {
+            isConsentSheetPresented = true
+        }
+    }
+    
+    var consentTask: ORKOrderedTask {
+        let consentInstructionStep = ORKInstructionStep(identifier: "ConsentInstructionStep")
+        consentInstructionStep.title = "Study Consent"
+        consentInstructionStep.detailText = """
+        In the next two steps you will be asked to review two consent forms the LifeSpace study. 
+        
+        Please read the forms carefully and sign if you agree to the terms.
+        """
+        
+        let consentDocument = LifeSpaceConsent()
+        let signature = consentDocument.signatures?.first
+        
+        let hipaaAuthorizationDocument = HIPAAAuthorization()
+        let hipaaSignature = hipaaAuthorizationDocument.signatures?.first
+        
+        let reviewConsentStep = ORKConsentReviewStep(
+            identifier: "ConsentReviewStep",
+            signature: signature,
+            in: consentDocument
         )
+        reviewConsentStep.text = "Review Consent Form"
+        reviewConsentStep.reasonForConsent = "Consent to join the LifeSpace Study."
+        
+        let reviewHIPAAAuthorizationStep = ORKConsentReviewStep(
+            identifier: "HIPAAAuthorizationReviewStep",
+            signature: hipaaSignature,
+            in: hipaaAuthorizationDocument
+        )
+        reviewHIPAAAuthorizationStep.text = "Review Consent Form"
+        reviewHIPAAAuthorizationStep.reasonForConsent = "Consent to join the LifeSpace Study."
+        
+        let steps = [consentInstructionStep, reviewConsentStep, reviewHIPAAAuthorizationStep]
+        
+        return ORKOrderedTask(identifier: "ConsentTask", steps: steps)
     }
 }
 
