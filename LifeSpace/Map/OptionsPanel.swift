35eeb56ac7bbe41e0b0c09506256f301910a1312
Vishnu Ravi <vishnur@stanford.edu>
2024-07-13 06:39:32 -0400

    Improve survey workflow (#31)

--- LifeSpace/Map/OptionsPanel.swift
+++ LifeSpace/Map/OptionsPanel.swift
@@ -10,6 +10,17 @@ import ResearchKitSwiftUI
 import Spezi
 import SwiftUI
 
+
+struct ButtonGroupBoxStyle: GroupBoxStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.content
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 8).fill(Color("AccentColor")))
+    }
+}
+
+
 struct OptionsPanel: View {
     @AppStorage(StorageKeys.trackingPreference) private var trackingOn = true
     @Environment(LocationModule.self) private var locationModule
@@ -18,6 +29,7 @@ struct OptionsPanel: View {
     
     @State private var showingSurveyAlert = false
     @State private var showingSurvey = false
+    @State private var showingStartSurveyModal = false
     
     var body: some View {
         GroupBox {
@@ -25,18 +37,61 @@ struct OptionsPanel: View {
                 self.showingSurvey.toggle()
             } label: {
                 Text("OPTIONS_PANEL_SURVEY_BUTTON")
+                    .bold()
+                    .foregroundColor(.white)
                     .frame(maxWidth: .infinity)
             }
             .sheet(isPresented: $showingSurvey) {
                 DailySurveyTaskView(showingSurvey: $showingSurvey)
             }
+            .sheet(isPresented: $showingStartSurveyModal) {
+                startSurveyModal
+                    .presentationDetents([.medium, .large])
+                    .presentationDragIndicator(.hidden)
+            }
             .onChange(of: scenePhase) { _, newPhase in
                 if newPhase == .active {
                     Task {
-                        await standard.getLatestSurveyDate()
+                        _ = await standard.getLatestSurveyDate()
+                        launchSurvey()
                     }
                 }
             }
+        }.groupBoxStyle(ButtonGroupBoxStyle())
+    }
+    
+    private var startSurveyModal: some View {
+        VStack {
+            Text("SURVEY_READY_QUESTION")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Button(action: {
+                self.showingSurvey = true
+                self.showingStartSurveyModal = false
+            }, label: {
+                Text("YES")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+            })
+            .padding()
+            .buttonStyle(.borderedProminent)
+            
+            Button(action: {
+                self.showingStartSurveyModal = false
+            }, label: {
+                Text("NO")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+            })
+            .padding()
+            .buttonStyle(.bordered)
+        }
+    }
+    
+    private func launchSurvey() {
+        if SurveyModule.shouldShowSurvey && !SurveyModule.surveyAlreadyTaken {
+            self.showingStartSurveyModal = true
         }
     }
 }
---
+++ LifeSpace/Survey/SurveyModule.swift
@@ -0,0 +1,50 @@
+//
+//  SurveyModule.swift
+//  LifeSpace
+//
+//  Created by Vishnu Ravi on 7/12/24.
+//
+
+import Foundation
+
+
+enum SurveyModule {
+    static var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyyy"
+        return formatter
+    }
+    
+    static var currentHour: Int {
+        Calendar.current.component(.hour, from: Date())
+    }
+    
+    static var isPreviousDaySurvey: Bool {
+        /// If the user is taking the survey before 7am, they should be informed that they are taking the
+        /// previous day's survey
+        currentHour < 7
+    }
+    
+    static var surveyAlreadyTaken: Bool {
+        let lastSurveyDateString = UserDefaults.standard.string(forKey: StorageKeys.lastSurveyDate)
+        
+        // Determine the survey date based on the current time
+        let surveyDate: Date
+        if currentHour < 7 {
+            surveyDate = Calendar.current.date(byAdding: .day, value: -1, to: Date())?.startOfDay ?? Date().startOfDay
+        } else {
+            surveyDate = Date().startOfDay
+        }
+        
+        // Format the survey date to a string
+        let surveyDateString = dateFormatter.string(from: surveyDate)
+        
+        // Compare the last survey date with the calculated survey date
+        return lastSurveyDateString == surveyDateString
+    }
+
+    static var shouldShowSurvey: Bool {
+        /// The survey should only be shown if it between 7pm and 7am
+        currentHour < 7 || currentHour >= 19
+    }
+}
