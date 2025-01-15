540fd1b695a5f32ede2377cde99412305075f1fc
Vishnu Ravi <vishnur@stanford.edu>
2025-01-15 15:42:57 -0500

    Update wording of survey questions (#58)

--- LifeSpace/Survey/DailySurveyTask.swift
+++ LifeSpace/Survey/DailySurveyTask.swift
@@ -51,7 +51,7 @@ class DailySurveyTask: ORKOrderedTask {
         let question3Step = ORKQuestionStep(
             identifier: "EmotionalWellBeingQuestion",
             title: "Emotional Well-being",
-            question: "I was happy",
+            question: "Consider your day today and evaluate your agreement with the following statement:\n\nI was happy.",
             answer: answerFormat3
         )
         steps.append(question3Step)
@@ -67,7 +67,7 @@ class DailySurveyTask: ORKOrderedTask {
         let question4Step = ORKQuestionStep(
             identifier: "PhysicalWellBeingQuestion",
             title: "Physical Well-being",
-            question: "Consider your day today and evaluate your agreement with the following statement.",
+            question: "Consider your day today and evaluate your agreement with the following statement:\n\nI felt fatigued.",
             answer: answerFormat4
         )
         steps.append(question4Step)
